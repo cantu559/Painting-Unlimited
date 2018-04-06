a802276e490b28b5702643e25214ecff1dd79f08
Anthony Cantu <cantu22.ac@gmail.com>
2018-04-05 18:29:48 -0700

    fixed mobile what we do

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+
+// functions for website and tablet-----------------------------------
+$(document).ready(function(){
+    $("#flip1").click(function(){
+        $("#panel1").slideToggle("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#flip2").click(function(){
+        $("#panel2").slideToggle("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#flip3").click(function(){
+        $("#panel3").slideToggle("slow");
+    });
+});
+
+
+//------------------------------------------------------------------------------
+// functions for mobile site----------------------------------------------------
+
+$(document).ready(function(){
+    $("#mflip1").click(function(){
+        $("#mpanel1").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#mflip2").click(function(){
+        $("#mpanel2").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#mflip3").click(function(){
+        $("#mpanel3").slideToggle("slow");
+    });
+});
+
+//----------------------------------------------------------------------------
